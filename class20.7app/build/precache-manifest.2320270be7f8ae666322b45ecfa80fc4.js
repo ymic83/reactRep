f54e75d031572b0e31834e360fbbc315a65b8014
ymic83 <ymic83@gmail.com>
2020-07-20 23:44:23 +0300

    yet another try

---
+++ class20.7app/build/precache-manifest.2320270be7f8ae666322b45ecfa80fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92ab80c748e2a57fe089d97c978ea82",
+    "url": "/reactRep/index.html"
+  },
+  {
+    "revision": "ea230448782fd2cb07b9",
+    "url": "/reactRep/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cfc61e8310bdffc2474",
+    "url": "/reactRep/static/js/2.512af8b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactRep/static/js/2.512af8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea230448782fd2cb07b9",
+    "url": "/reactRep/static/js/main.93737891.chunk.js"
+  },
+  {
+    "revision": "95db79bfcc0931f70f33",
+    "url": "/reactRep/static/js/runtime-main.5dc35499.js"
+  }
+]);
