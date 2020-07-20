bdd015d5855667b298e055c0375870bc18567e08
ymic83 <ymic83@gmail.com>
2020-07-20 23:36:33 +0300

    Trying Again

---
+++ class20.7app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+    </div>
+  );
+}
+
+export default App;
