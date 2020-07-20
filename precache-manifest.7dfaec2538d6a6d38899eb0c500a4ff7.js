e4f4345c30d3b613757a2182a434086f4a92a4b8
ymic83 <ymic83@gmail.com>
2020-07-20 23:37:38 +0300

    Updates

---
+++ precache-manifest.7dfaec2538d6a6d38899eb0c500a4ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57edebee57f41921b75330c99166e326",
+    "url": "/nameOfReposetory/index.html"
+  },
+  {
+    "revision": "c49d508e3771fe10b1ed",
+    "url": "/nameOfReposetory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cfc61e8310bdffc2474",
+    "url": "/nameOfReposetory/static/js/2.512af8b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nameOfReposetory/static/js/2.512af8b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49d508e3771fe10b1ed",
+    "url": "/nameOfReposetory/static/js/main.8363f176.chunk.js"
+  },
+  {
+    "revision": "370829ed9273aa08fcaa",
+    "url": "/nameOfReposetory/static/js/runtime-main.66e8db94.js"
+  }
+]);
